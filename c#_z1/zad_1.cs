9c83ee1983d2922ac25f43ecba0f243ecee8a9bf
2221121-26 <146703405+2221121-26@users.noreply.github.com>
2024-06-09 13:08:46 +0300

    first task ready

---
+++ c#_z1/zad_1.cs
@@ -0,0 +1,29 @@
+﻿namespace C_tasks
+{
+    public static class zad_1
+    {
+        static void Main(string[] args)
+        {
+            string c = Console.ReadLine();
+            if (c.Length % 2 == 0)
+            {
+                Console.WriteLine($"{perevorot(c[..(c.Length / 2)]) + perevorot(c.Substring(c.Length / 2))}");
+            }
+            else
+            {
+                Console.WriteLine($"{perevorot(c) + c}");
+            }
+
+
+            static string perevorot(string message)
+            {
+                string hc = "";
+                foreach (var ch in message)
+                {
+                    hc = ch + hc;
+                }
+                return hc;
+            }
+        }
+    }
+}
